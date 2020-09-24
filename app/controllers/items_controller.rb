452fdafd9e3d33c494a3376830248cf93754903d
kaichihasegawa1220 <sayakaichi@icloud.com>
2020-09-25 00:49:05 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -24,8 +24,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destory
+    @item.destory
+    redirect_to root_path
+  end
+
   private
   def item_params
-    params.require(:item).permit(:image, :name, :text, :genre_id, :condition_id, :payment_id, :shipping_location_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  params.require(:item).permit(:image, :name, :text, :genre_id, :condition_id, :payment_id, :shipping_location_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
