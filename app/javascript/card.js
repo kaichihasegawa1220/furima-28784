c67b84b8a5f6aa9a8c917abd7887f3188c15e5c4
kaichihasegawa1220 <sayakaichi@icloud.com>
2020-10-09 10:54:03 +0900

    商品購入機能　修正３

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,7 @@
 const pay = ()=> {
   const PAYJP_PUBLIC_KEY = process.env.PAYJP_PUBLIC_KEY;
   Payjp.setPublicKey(PAYJP_PUBLIC_KEY);// PAY.JPテスト公開鍵
+  console.log(PAYJP_PUBLIC_KEY)
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
